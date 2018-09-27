db655fdc641e2f49f93cbbace7fd0bc4cc2bed88
Enter <350840291@qq.com>
2018-09-27 18:43:04 +0800

    init

---
+++ src/Ocelot.Provider.Etcd/IConsulClientFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Ocelot.Provider.Etcd
+{
+    using global::Consul;
+
+    public interface IConsulClientFactory
+    {
+        IConsulClient Get(ConsulRegistryConfiguration config);
+    }
+}
---
+++ src/Ocelot.Provider.Etcd/UnableToSetConfigInConsulError.cs
@@ -0,0 +1,12 @@
+﻿namespace Ocelot.Provider.Etcd
+{
+    using Errors;
+
+    public class UnableToSetConfigInConsulError : Error
+    {
+        public UnableToSetConfigInConsulError(string s) 
+            : base(s, OcelotErrorCode.UnknownError)
+        {
+        }
+    }
+}
